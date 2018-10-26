db93baad23bc831126faedccc157f4eccc23c318
Alex Janci <alex@alexs-macbook-pro.local>
2018-10-26 17:06:40 -0500

    Initial Commit

---
+++ QuestionBot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuestionBot
+//
+//  Created by Alex Janci on 10/26/18.
+//  Copyright © 2018 Alex Janci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
