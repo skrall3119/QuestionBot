63c32a107928be793dadf24ac4a898b31d41ecc7
Alex Janci <alex@alexs-macbook-pro.local>
2018-10-27 16:58:31 -0500

    completed the question bot program

--- QuestionBot/ViewController.swift
+++ QuestionBot/ViewController.swift
@@ -1,20 +1,61 @@
-//
-//  ViewController.swift
-//  QuestionBot
-//
-//  Created by Alex Janci on 10/26/18.
-//  Copyright © 2018 Alex Janci. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var responseLabel: UILabel!
+    @IBOutlet weak var askButton: UIButton!
+    @IBOutlet weak var questionField: UITextField!
+    
+    let questionAnswerer = MyQuestionAnswerer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionField.becomeFirstResponder()
     }
 
+    func respondToQuestion(_ question: String) {
+        let answer = questionAnswerer.responseTo(question: question)
+
+        displayAnswerTextOnScreen(answer)
+        questionField.placeholder = "Ask another question..."
+        questionField.text = nil
+        askButton.isEnabled = false
+    }
+
+    @IBAction func askButtonTapped(_ sender: AnyObject) {
+        guard questionField.text != nil else {
+            return
+        }
+        questionField.resignFirstResponder()
+    }
+    
+    func displayAnswerTextOnScreen(_ answer: String) {
+        responseLabel.text = answer
+    }
 
 }
 
+extension ViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text else {
+            return
+        }
+        
+        respondToQuestion(text)
+    }
+    
+    @IBAction func editingChanged(_ textField: UITextField) {
+        guard let text = textField.text else {
+            askButton.isEnabled = false
+            return
+        }
+        
+        askButton.isEnabled = !text.isEmpty
+    }
+}
